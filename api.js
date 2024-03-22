d7090132f36f3f920a063b0fd62817cb0aeb97c6
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-03-22 18:41:56 +0000

    added custom logging, using daisyUI.

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
 const p = require("primebit.js");
+const path = require("path")
 
 const app = express();
 
