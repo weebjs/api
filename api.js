94762c77b878981230eb6408800bf81c5f6bf07e
Tahfari Chin <130629753+weebjs@users.noreply.github.com>
2024-03-22 21:57:06 -0400

    Update api.js

--- api.js
+++ api.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const express = require('express');
-const bodyParser = require('body-parser');
 const axios = require('axios');
 const p = require("primebit.js");
-const path = require("path");
+const path = require("path")
 
 const app = express();
-
-const options = process.argv[2]; // Access the first command-line argument
-
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'website')));
 
 app.get('/animememes', async (req, res) => {
   try {
-    const response = await axios.get(`https://meme-api.com/gimme/${options}`);
+    const response = await axios.get('https://meme-api.com/gimme/animememes');
     const { postLink, subreddit, title, url, author, ups } = response.data;
     const filter = { postLink, subreddit, title, url, author, ups };
     res.json(filter);
   } catch (error) {
     res.status(500).json({ code: "500", error: 'an error occurred while fetching the anime meme.' });
+  }
+});
+
+app.get('/meme', async (req, res) => {
+  try {
+    const response = await axios.get('https://meme-api.com/gimme/memes');
+    const { postLink, subreddit, title, url, author, ups } = response.data;
+    const filter = { postLink, subreddit, title, url, author, ups };
+    res.json(filter);
+  } catch (error) {
+    res.status(500).json({ code: "500", error: 'an error occurred while fetching the meme.' });
   }
 });
 
